3d08b7099acab317620540303a351b2472600ace
fish003 <95459216+fish003@users.noreply.github.com>
2024-09-10 12:15:42 +0800

    Update index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -13,6 +13,13 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  mounted() {
+    console.log(process.env.VUE_APP_BASE_API,'appindex');
+    console.log(process,'appindex1');
+    console.log(process.env,'appindexenv');
+    console.log(process.env.VUE_APP_TEST_KEY_ONE,'appindexone');
+    console.log(process.env.VUE_APP_TEST_KEY_TWO,'appindextwo');
   }
 }
 </script>
